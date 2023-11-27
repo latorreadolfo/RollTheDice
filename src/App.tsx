ed7db65a9150a02d3433a2832066d5589b11b90c
latorreadolfo <adolfo3eng@gmail.com>
2023-11-27 18:22:25 -0300

    image background and roll the dice feature

---
+++ src/App.tsx
@@ -0,0 +1,112 @@
+import React, {useState} from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  ImageSourcePropType,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+  ImageBackground,
+} from 'react-native';
+
+import DiceOne from '../assets/One.png';
+import DiceTwo from '../assets/Two.png';
+import DiceThree from '../assets/Three.png';
+import DiceFour from '../assets/Four.png';
+import DiceFive from '../assets/Five.png';
+import DiceSix from '../assets/Six.png';
+
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType;
+}>;
+
+const Dice = ({imageUrl}: DiceProps): JSX.Element => {
+  return (
+    <View>
+      <Image style={styles.diceImage} source={imageUrl} />
+    </View>
+  );
+};
+function App(): JSX.Element {
+  const [diceImage, setDiceImage] = useState<ImageSourcePropType>(DiceOne);
+
+  const rollDiceOnTap = () => {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+
+    switch (randomNumber) {
+      case 1:
+        setDiceImage(DiceOne);
+        break;
+      case 2:
+        setDiceImage(DiceTwo);
+        break;
+      case 3:
+        setDiceImage(DiceThree);
+        break;
+      case 4:
+        setDiceImage(DiceFour);
+        break;
+      case 5:
+        setDiceImage(DiceFive);
+        break;
+      case 6:
+        setDiceImage(DiceSix);
+        break;
+
+      default:
+        setDiceImage(DiceOne);
+        break;
+    }
+  };
+
+  return (
+    <ImageBackground
+      style={styles.imgBg}
+      source={{
+        uri: 'https://bordalo.observador.pt/v2/q:84/rs:fill:1280:718/c:1280:718:nowe:0:144/plain/https://s3.observador.pt/wp-content/uploads/2023/09/19152125/melhores-casinos-online.jpg',
+      }}>
+      <View style={styles.container}>
+        <Dice imageUrl={diceImage} />
+        <Pressable onPress={rollDiceOnTap}>
+          <Text style={styles.rollDiceBtnText}>Roll the dice</Text>
+        </Pressable>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+  diceContainer: {
+    margin: 12,
+  },
+  imgBg: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+  rollDiceBtnText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#E5E0FF',
+    fontSize: 16,
+    color: '#8EA7E9',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+    backgroundColor: 'rgba(255, 255, 255, 0.75)',
+  },
+});
+
+export default App;
